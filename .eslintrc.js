b90d0c0fbe01d6f0065fe2ffc6a6bf5bc59299a0
dante <dantesyh123@qq.com>
2022-12-17 11:48:19 +0800

    [个人博客开发] 格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "vue/comment-directive": "off"
-    }
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'vue/comment-directive': 'off'
+  }
 }
